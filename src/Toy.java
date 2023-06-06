66569d47b89137ba15727df56cb24149902aac59
mexatron <mexatron>
2023-06-07 01:08:16 +0200

    add class Toy

---
+++ src/Toy.java
@@ -0,0 +1,13 @@
+public class Toy {
+    String name;
+    private int id;
+    int quantity;
+    int weight;
+
+    public Toy(String name,int quantity,int weight)
+    {
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+}
