5e372fc48845d56460671a111b3e05a0d2192ea4
mexatron <mexatron>
2023-06-07 01:15:07 +0200

    add class IdGenerator

--- src/Toy.java
+++ src/Toy.java
@@ -1,13 +1,37 @@
 public class Toy {
     String name;
     private int id;
-    int quantity;
-    int weight;
+    private int quantity;
+    private int weight;
 
     public Toy(String name,int quantity,int weight)
     {
         this.name = name;
         this.quantity = quantity;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
         this.weight = weight;
     }
 }
